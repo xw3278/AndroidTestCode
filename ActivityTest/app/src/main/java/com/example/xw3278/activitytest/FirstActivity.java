65893bfd11531a35f7567d3da1dd7290022d94ab
xw3278 <xw3278@163.com>
2017-08-31 11:55:17 +0800

    add anthor project

---
+++ ActivityTest/app/src/main/java/com/example/xw3278/activitytest/FirstActivity.java
@@ -0,0 +1,87 @@
+package com.example.xw3278.activitytest;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button_one = (Button)findViewById(R.id.button_1);
+        button_one.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //1. toast
+                //Toast.makeText(FirstActivity.this, "You clicked button 1",
+                //        Toast.LENGTH_SHORT).show();
+                //2. finish
+                //finish();
+                //3. (1)Intent
+                //Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                //startActivity(intent);
+                // (2)Intent
+                //Intent intent = new Intent("com.example.xw3278.activitytest.ACTION_START");
+                //intent.addCategory("com.example.xw3278.activitytest.MY_CATEGORY");
+                //startActivity(intent);
+                //Intent intent = new Intent(Intent.ACTION_VIEW);
+                //intent.setData(Uri.parse("http://www.baidu.com"));
+                //startActivity(intent);
+                //Intent intent = new Intent(Intent.ACTION_DIAL);
+                //intent.setData(Uri.parse("tel:10086"));
+                //startActivity(intent);
+                //String data = "Hello secondActivity!";
+                //Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                //intent.putExtra("extra_data", data);
+                //startActivity(intent);
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+        //return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+        //return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
+    }
+}
---
+++ ActivityTest/app/src/main/java/com/example/xw3278/activitytest/SecondActivity.java
@@ -0,0 +1,39 @@
+package com.example.xw3278.activitytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        //Intent intent = getIntent();
+        //String data = intent.getStringExtra("extra_data");
+        //Log.d("SecondActivity", data);
+        Button button_two = (Button)findViewById(R.id.button_2);
+        button_two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "alex: hello FirstActivity!");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        Intent intent = new Intent();
+        intent.putExtra("data_return", "alex: hello FirstActivity!");
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
