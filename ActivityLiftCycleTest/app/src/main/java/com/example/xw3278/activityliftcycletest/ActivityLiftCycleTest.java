dbde28c30a35b804b27f880c78c9aad64af9b8f4
xw3278 <xw3278@163.com>
2017-08-31 11:49:42 +0800

    add 2 project for android

---
+++ ActivityLiftCycleTest/app/src/main/java/com/example/xw3278/activityliftcycletest/ActivityLiftCycleTest.java
@@ -0,0 +1,71 @@
+package com.example.xw3278.activityliftcycletest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivityLiftCycleTest extends AppCompatActivity {
+    public static final String TAG = "Main Activity!";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lift_cycle_test);
+        Log.d(TAG, "onCreate");
+        setContentView(R.layout.activity_lift_cycle_test);
+        Button startNormalActivity = (Button) findViewById(R.id.start_normal_activity);
+        Button startDialogActivity = (Button) findViewById(R.id.start_dialog_activity);
+        startNormalActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityLiftCycleTest.this, NormalActivity.class);
+                startActivity(intent);
+            }
+        });
+        startDialogActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityLiftCycleTest.this, DialogActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+}
