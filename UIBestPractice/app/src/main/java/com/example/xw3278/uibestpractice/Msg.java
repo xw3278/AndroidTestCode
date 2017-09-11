4ad111b4466a4e17a3534bfebf8516f4175c8dac
xw3278 <xw3278@163.com>
2017-09-11 15:06:09 +0800

    add ui best practice project

---
+++ UIBestPractice/app/src/main/java/com/example/xw3278/uibestpractice/Msg.java
@@ -0,0 +1,28 @@
+package com.example.xw3278.uibestpractice;
+
+/**
+ * Created by Administrator on 2017/9/11.
+ */
+
+public class Msg {
+    public static final int TYPE_RECEIVED = 0;
+
+    public static final int TYPE_SENT = 1;
+
+    private String content;
+
+    private int type;
+
+    public Msg(String content, int type) {
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getType() {
+        return type;
+    }
+}
