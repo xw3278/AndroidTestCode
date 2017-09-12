7f5cd041cc593e359e04a0e51197e273d84befd2
xw3278 <xw3278@163.com>
2017-09-12 17:56:34 +0800

    add fragment test project

---
+++ FragmentTest/app/src/main/java/com/example/xw3278/fragmenttest/AnotherRightFragment.java
@@ -0,0 +1,20 @@
+package com.example.xw3278.fragmenttest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/9/12.
+ */
+
+public class AnotherRightFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.another_right_fragment, container, false);
+        return view;
+    }
+}
---
+++ FragmentTest/app/src/main/java/com/example/xw3278/fragmenttest/RightFragment.java
@@ -0,0 +1,21 @@
+package com.example.xw3278.fragmenttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/9/12.
+ */
+
+public class RightFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.right_fragment, container, false);
+        return view;
+    }
+}
