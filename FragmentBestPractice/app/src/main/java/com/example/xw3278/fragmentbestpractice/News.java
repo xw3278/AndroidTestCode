8a903b2f4b86c04c2ec2751ee3454c9266856144
xw3278 <xw3278@163.com>
2017-09-13 23:31:30 +0800

    add fragment best practice

---
+++ FragmentBestPractice/app/src/main/java/com/example/xw3278/fragmentbestpractice/News.java
@@ -0,0 +1,22 @@
+package com.example.xw3278.fragmentbestpractice;
+
+/**
+ * Created by Administrator on 2017/9/13.
+ */
+
+public class News {
+    private String title;
+    private String content;
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ FragmentBestPractice/app/src/main/java/com/example/xw3278/fragmentbestpractice/NewsContentActivity.java
@@ -0,0 +1,13 @@
+package com.example.xw3278.fragmentbestpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_content);
+    }
+}
