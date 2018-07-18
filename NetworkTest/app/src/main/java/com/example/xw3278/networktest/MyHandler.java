330738adf153b428c4aacac5d809f74e2e9e4bb9
xw3278 <xw3278@163.com>
2018-07-18 08:06:45 +0800

    update

--- NetworkTest/app/src/main/java/com/example/xw3278/networktest/MyHandler.java
+++ NetworkTest/app/src/main/java/com/example/xw3278/networktest/MyHandler.java
@@ -1,31 +1,54 @@
 package com.example.xw3278.networktest;
 
+import android.util.Log;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class MyHandler extends DefaultHandler {
+    private String nodeName;
+    private StringBuilder id;
+    private StringBuilder name;
+    private StringBuilder version;
+
     @Override
     public void startDocument() throws SAXException {
+        id = new StringBuilder();
+        name = new StringBuilder();
+        version = new StringBuilder();
     }
     @Override
     public void startElement(String uri, String localName, String qName,Attributes attributes)
         throws SAXException {
-
+        nodeName = localName;
     }
 
     @Override
     public void characters(char[] ch, int start, int length)throws SAXException{
-
+        if ("id".equals(nodeName)) {
+            id.append(ch, start, length);
+        } else if ("name".equals(nodeName)){
+            name.append(ch, start, length);
+        } else if ("version".equals(nodeName)){
+            version.append(ch, start, length);
+        }
     }
 
     @Override
     public void endElement(String uri, String localName, String qName)throws
             SAXException {
-
+        if ("app".equals(localName)){
+            Log.d("Myhandler", "id is " + id.toString().trim());
+            Log.d("Myhandler", "name is " + name.toString().trim());
+            Log.d("Myhandler", "version is " + version.toString().trim());
+            id.setLength(0);
+            name.setLength(0);
+            version.setLength(0);
+        }
     }
     @Override
     public void endDocument() throws SAXException {
-
+        super.endDocument();
     }
 }
